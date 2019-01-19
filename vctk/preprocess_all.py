673b654feac3ca58747801d191dc9f146bf0de59
Dmytro Tkanov <akademi4eg@gmail.com>
2019-01-19 18:22:48 +0200

    reworked features extraction

--- vctk/preprocess_all.py
+++ vctk/preprocess_all.py
@@ -1,22 +1,41 @@
 from collections import Counter
 import librosa
+import tensorflow as tf
 import numpy as np
-from joblib import Parallel, delayed
+from tqdm import tqdm
+from multiprocessing import Lock
+from joblib import Parallel, delayed, dump
+from argparse import ArgumentParser
 
 from utils import calculate_mfcc_op
 
 
-VOCAB_FILENAME = './data/vocab.txt'
 SAMPLE_RATE = 16000
-N_MFCC = 13
-WINDOW = int(.02 * SAMPLE_RATE)
-STEP = WINDOW // 2
-N_MELS = 40
-ALPHA = 0.9
 
 vocabulary = Counter()
-running_mean = np.zeros(1, N_MFCC)
-running_std = np.zeros(1, N_MFCC)
+means = None
+stds = None
+total = 0
+par_handle = None
+session = tf.Session()
+mutex = Lock()
+waveform_place = tf.placeholder(tf.float32, [None, None])
+mfcc_op = None
+
+
+def make_example(input, label):
+    feature_lists = tf.train.FeatureLists(feature_list={
+        'labels': tf.train.FeatureList(feature=[
+            tf.train.Feature(bytes_list=tf.train.BytesList(value=[p.encode()]))
+            for p in label
+        ]),
+        'inputs': tf.train.FeatureList(feature=[
+            tf.train.Feature(float_list=tf.train.FloatList(value=f))
+            for f in input
+        ])
+    })
+
+    return tf.train.SequenceExample(feature_lists=feature_lists)
 
 
 def read_audio_and_text(inputs):
@@ -33,32 +52,67 @@ def read_audio_and_text(inputs):
     }
 
 
-def build_features_and_vocabulary_fn(inputs):
-    global running_mean, running_std
+def build_features_and_vocabulary_fn(args, inputs):
+    global means, stds, total
     waveform = inputs['waveform']
     text = inputs['text']
-    mfcc = calculate_mfcc_op(SAMPLE_RATE, N_MFCC, WINDOW, STEP, N_MELS)(waveform)
+    mfcc = session.run(mfcc_op, {waveform_place: waveform[np.newaxis, :]})[0, :, :]
     vocabulary.update(text)
-    running_mean = ALPHA * running_mean + (1 - ALPHA) * np.mean(mfcc, axis=0)
-    running_std = ALPHA * running_std + (1 - ALPHA) * np.std(mfcc, axis=0)
+    if means is None:
+        means = np.mean(mfcc, axis=0)
+        stds = np.std(mfcc, axis=0)
+    else:
+        means += np.mean(mfcc, axis=0)
+        stds += np.std(mfcc, axis=0)
+    total += 1
     return {
         'mfcc': mfcc,
         'text': text
     }
 
 
+def write_tf_output(writer, inputs):
+    with mutex:
+        writer.write(make_example(inputs['mfcc'], inputs['text']).SerializeToString())
+    par_handle.update()
+
+
+def process_line(args, writer, line):
+    filename, text = line.split(',')
+    inputs = {
+        'file_path': filename,
+        'text': text.strip()
+    }
+    out = read_audio_and_text(inputs)
+    out = build_features_and_vocabulary_fn(args, out)
+    write_tf_output(writer, out)
+
+
 if __name__ == "__main__":
-    print('Processing audio dataset.')
-    fake_data = [
-        {
-            'file_path': 'data/VCTK-Corpus/wav48/p340/p340_047.wav',
-            'text': 'I felt under a lot of pressure.'
-        },
-        {
-            'file_path': 'data/VCTK-Corpus/wav48/p340/p340_210.wav',
-            'text': 'I had just taken it off.'
-        }
-    ]
-    a = read_audio_and_text(fake_data[0])
-    b = build_features_and_vocabulary_fn(a)
-    print(b)
+    parser = ArgumentParser()
+    parser.add_argument('--input_file', help='File with audio paths and texts.', required=True)
+    parser.add_argument('--output_file', help='Target TFRecord file name.', required=True)
+    parser.add_argument('--norm_file', help='File name for normalization data.', default=None)
+    parser.add_argument('--vocab_file', help='Vocabulary file name.', default=None)
+    parser.add_argument('--top_k', help='Max size of vocabulary.', type=int, default=1000)
+    parser.add_argument('--n_mfcc', help='Number of MFCC coeffs.', type=int, default=13)
+    parser.add_argument('--n_mels', help='Number of mel-filters.', type=int, default=40)
+    parser.add_argument('--window', help='Analysis window length in ms.', type=int, default=20)
+    parser.add_argument('--step', help='Analysis window step in ms.', type=int, default=10)
+    args = parser.parse_args()
+    print('Processing audio dataset from file {}.'.format(args.input_file))
+    window = int(SAMPLE_RATE * args.window / 1000.0)
+    step = int(SAMPLE_RATE * args.step / 1000.0)
+    mfcc_op = calculate_mfcc_op(SAMPLE_RATE, args.n_mfcc, window, step, args.n_mels)(waveform_place)
+    lines = open(args.input_file, 'r').readlines()
+    par_handle = tqdm(unit='sound')
+    with tf.io.TFRecordWriter(args.output_file) as writer:
+        Parallel(n_jobs=4, prefer="threads")(delayed(process_line)(args, writer, x) for x in lines)
+    session.close()
+    par_handle.close()
+    if args.norm_file is not None:
+        dump([means / total, stds / total], args.norm_file)
+    if args.vocab_file is not None:
+        with open(args.vocab_file, 'w') as f:
+            for x, _ in vocabulary.most_common(args.top_k):
+                f.write(x + '\n')
