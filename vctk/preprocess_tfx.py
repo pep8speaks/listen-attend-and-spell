3693c284fbf60e75c9071dc7ede0da10a6582517
Dmytro Tkanov <akademi4eg@gmail.com>
2019-01-19 13:48:38 +0200

    updates

--- vctk/preprocess_tfx.py
+++ vctk/preprocess_tfx.py
@@ -15,9 +15,15 @@ N_MELS = 40
 
 def read_audio_and_text(inputs):
     audio_path = inputs['file_path']
+    text = inputs['text']
+    text = ' '.join(text.split())
+    for p in ',.:;?!-_':
+        text = text.replace(p, '')
+    text = text.lower().split()
     audio, _ = librosa.load(audio_path, sr=SAMPLE_RATE, mono=True)
     return {
-        'waveform': audio
+        'waveform': audio,
+        'text': text
     }
 
 
@@ -26,7 +32,7 @@ def build_features_and_vocabulary_fn(inputs):
     text = inputs['text']
     mfcc = calculate_mfcc_op(SAMPLE_RATE, N_MFCC, WINDOW, STEP, N_MELS)(waveform)
     mfcc_normalized = tft.scale_to_z_score(mfcc, name='mfcc_normalization')
-    tft.vocabulary(text, top_k=10000, vocab_filename=VOCAB_FILENAME, name='vocabulary')
+    # tft.vocabulary(text, top_k=10000, vocab_filename=VOCAB_FILENAME, name='vocabulary')
     return {
         'mfcc': mfcc_normalized,
         'text': text
@@ -35,3 +41,16 @@ def build_features_and_vocabulary_fn(inputs):
 
 if __name__ == "__main__":
     print('Processing audio dataset.')
+    fake_data = [
+        {
+            'file_path': 'data/VCTK-Corpus/wav48/p340/p340_047.wav',
+            'text': 'I felt under a lot of pressure.'
+        },
+        {
+            'file_path': 'data/VCTK-Corpus/wav48/p340/p340_210.wav',
+            'text': 'I had just taken it off.'
+        }
+    ]
+    a = read_audio_and_text(fake_data[0])
+    b = build_features_and_vocabulary_fn(a)
+    print(b)
