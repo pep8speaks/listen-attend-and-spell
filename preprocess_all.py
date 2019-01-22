fix for normalization calculation with multiple threads

preprocess_all.py
@@ -18,7 +18,8 @@ stds = None
 total = 0
 par_handle = None
 session = tf.Session()
-mutex = Lock()
+tfrecord_mutex = Lock()
+stats_mutex = Lock()
 waveform_place = tf.placeholder(tf.float32, [None, None])
 mfcc_op = None
 
@@ -63,13 +64,14 @@ def build_features_and_vocabulary_fn(args, inputs):
         text = list(get_ipa(' '.join(text), language))
     mfcc = session.run(mfcc_op, {waveform_place: waveform[np.newaxis, :]})[0, :, :]
     vocabulary.update(text)
-    if means is None:
-        means = np.mean(mfcc, axis=0)
-        stds = np.std(mfcc, axis=0)
-    else:
-        means += np.mean(mfcc, axis=0)
-        stds += np.std(mfcc, axis=0)
-    total += 1
+    with stats_mutex:
+        if means is None:
+            means = np.mean(mfcc, axis=0)
+            stds = np.std(mfcc, axis=0)
+        else:
+            means += np.mean(mfcc, axis=0)
+            stds += np.std(mfcc, axis=0)
+        total += 1
     return {
         'mfcc': mfcc,
         'text': text
@@ -77,7 +79,7 @@ def build_features_and_vocabulary_fn(args, inputs):
 
 
 def write_tf_output(writer, inputs):
-    with mutex:
+    with tfrecord_mutex:
         writer.write(make_example(inputs['mfcc'], inputs['text']).SerializeToString())
     par_handle.update()
 